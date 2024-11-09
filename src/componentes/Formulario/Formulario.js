b63980bae420b650fc70bd5d7dc6510cf69f17f6
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2024-11-09 03:16:00 -0300

    Created Formulario
    create component Formulario

---
+++ src/componentes/Formulario/Formulario.js
@@ -0,0 +1,16 @@
+import CampoTexto from '../CampoTexto';
+import './Formulario.css';
+
+export const Formulario = () => {
+
+    return (
+        <section className="formulario">
+            <form>
+                <h1>Preencha os dados para criar o card do colaborador.</h1>
+                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
+                <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+            </form>
+        </section>
+    );
+}
