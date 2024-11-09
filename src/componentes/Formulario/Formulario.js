ff56a7e7e46b960f1c21d3e5c522d20742ae0822
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2024-11-09 15:16:07 -0300

    Update Formulario.js
    refatorando o componente Botao

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
@@ -22,7 +23,9 @@ export const Formulario = () => {
                 <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
                 <ListaSuspensa label="Times" itens={times} />
-                <button className="criar-card" type="submit">Criar Card</button>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     );
