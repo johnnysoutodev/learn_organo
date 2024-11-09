d85871aa146f6eb1b14772f85e80b26857c5bc9e
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2024-11-09 03:01:47 -0300

    Update App.js
    add componentes para visualizar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import Banner from './componentes/Banner';
 import CampoTexto from './componentes/CampoTexto';
 
 function App() {
+
   return (
     <div className="App">
       <Banner />
-      <CampoTexto />
+      <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
     </div>
   );
 }
