d13c1d8c78a5fe4d9e09654b19bd8d1665e8f8ca
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2024-11-09 02:42:14 -0300

    Update CampoTexto.js
    criando uma label do componente

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,8 +1,10 @@
+import './CampoTexto.css';
+
 export const CampoTexto = () => {
     return (
-        <div>
+        <div className="campo-texto">
             <label>Nome</label>
-            <input />
+            <input placeholder='Digite o seu nome'/>
         </div>
     );
 }
